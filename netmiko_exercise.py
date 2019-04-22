f6d058db7dd612c22f619a24e76c46dd618ea478
Kelcey Decker <kelcey.decker@gmail.com>
2019-04-22 15:18:11 -0700

    Added comments to Netmiko exercise.

--- netmiko_exercise.py
+++ netmiko_exercise.py
@@ -3,8 +3,10 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
+# Prompt user for password to lab devices...
 lab_password = getpass("Enter password for lab devices: ")
 
+# Lab device information:
 nxos1 = {
 	'device_type': 'cisco_nxos',
 	'host': 'nxos1.lasthop.io',
@@ -21,11 +23,13 @@ nxos2 = {
 
 devices = [ nxos1, nxos2 ]
 
+# Log into each device and display the prompt for each:
 for switch in devices:
 	myLabHandler = ConnectHandler(**switch)
 	output = myLabHandler.find_prompt()
 	print(output)
 
+# On the last device, save off the 'show version' output to a log file:
 show_version_output = myLabHandler.send_command("show version")
 
 show_version_logfile = open('netmiko_exercise_output.log', 'w')
